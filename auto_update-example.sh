11884eef94e4de6b6f62678e5bc39a151d8c7db1
Jason Brodeur <jason.brodeur@gmail.com>
2020-02-29 14:46:42 -0500

    updated example

--- auto_update-example.sh
+++ auto_update-example.sh
@@ -3,16 +3,23 @@
 echo "Running survival guide update"
 cd /media/Stuff/Working/portage-ceg.github.io/
 
+
+echo "Pulling changes"
+git pull -u https://<username>:<password>@github.com/portage-CEG/portage-ceg.github.io.git master
+
 # Run the python script (python3):
 /media/Stuff/Working/portage-ceg.github.io/csv_to_jekyll_portageceg.py
+
 echo "Adding new posts"
 git add _posts/*
-echo "Adding other changes"
+echo "Adding other local changes"
 git add -u
-echo "Commits:"
+
+echo "Committing"
 git commit -m 'automatic update'
-echo "Push to GitHub:"
+
+echo "Pushing to GitHub"
 #git push origin master
 git push -u https://<username>:<password>@github.com/portage-CEG/portage-ceg.github.io.git master
-echo "Update completed."
 
+echo "Update completed."
