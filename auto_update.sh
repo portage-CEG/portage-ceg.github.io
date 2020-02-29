23647cfe02f1aaae5fbd994313dd04ddcca32f93
Jason Brodeur <jason.brodeur@gmail.com>
2020-02-28 23:18:27 -0500

    added updater file

---
+++ auto_update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Running survival guide update"
+cd /media/Stuff/Working/portage-ceg.github.io/
+
+# Run the python script (python3):
+/media/Stuff/Working/portage-ceg.github.io/csv_to_jekyll_portageceg.py
+git add -u
+git commit -m 'automatic update'
+git push origin master
+ 
