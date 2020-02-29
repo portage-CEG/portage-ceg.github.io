5af41223a7f9c2d46f59d776af03ecd7c6c94031
Jason Brodeur <jason.brodeur@gmail.com>
2020-02-29 07:20:01 -0500

    automatic update

--- auto_update.sh
+++ auto_update.sh
@@ -5,8 +5,12 @@ cd /media/Stuff/Working/portage-ceg.github.io/
 
 # Run the python script (python3):
 /media/Stuff/Working/portage-ceg.github.io/csv_to_jekyll_portageceg.py
+echo "Adding new posts"
 git add _posts/*
+echo "Adding other changes"
 git add -u
+echo "Commits:"
 git commit -m 'automatic update'
+echo "Push to GitHub:"
 git push origin master
  
